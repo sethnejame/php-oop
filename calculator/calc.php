468089e2b6ea42871b366e988d6eabf4b2ce31ce
seth@mondido <sethnejame>
2019-10-08 09:03:09 +0200

    Adding calc class, adding calc vars

---
+++ calculator/calc.php
@@ -0,0 +1,9 @@
+<?php
+
+  $num1 = $_POST['num1'];
+  $num2 = $_POST['num2'];
+  $cal = $_POST['cal'];
+
+  
+
+?>
---
+++ calculator/includes/calc.inc.php
@@ -0,0 +1,16 @@
+<?php
+
+  class Calc {
+
+    public $num1;
+    public $num2;
+    public $cal;
+
+    public function __construct() {
+      $this->num1 = $num1;
+      $this->num2 = $num2;
+      $this->cal = $cal;
+    }
+
+  }
+?>
